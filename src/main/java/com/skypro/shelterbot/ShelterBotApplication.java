b0ba2b753a7483023b84ee69dafc6cf99bc807b3
Dianakyz <3222777my@gmail.com>
2023-04-16 19:33:36 +0300

    Добавила ссылку на свой GitHub

--- src/main/java/com/skypro/shelterbot/ShelterBotApplication.java
+++ src/main/java/com/skypro/shelterbot/ShelterBotApplication.java
@@ -8,7 +8,6 @@ public class ShelterBotApplication {
 
     public static void main(String[] args) {
         SpringApplication.run (ShelterBotApplication.class,  args);
-        System.out.println("Hello world!");
     }
 
 }
