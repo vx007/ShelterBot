9c4ca556bc91f6f49a69b9890b4726bc09507cce
AlexandrLytkin <Dracca@mail.ru>
2023-04-17 23:19:25 +0300

    Добавил антотации @Slf4j и логи в методах которые могут выбросить ошибку, для дальньнейшего логирования, лам логер с параметрами не реализован в файле.xml. Добавил меню для бота с некоторыми вариантами ответов.

--- src/main/java/com/skypro/shelterbot/service/TelegramBot.java
+++ src/main/java/com/skypro/shelterbot/service/TelegramBot.java
@@ -1,19 +1,43 @@
 package com.skypro.shelterbot.service;
 
 import com.skypro.shelterbot.config.BotConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
 
     final BotConfig botConfig;
+    static final String HELP_TEXT = "Этот бот создан для реализации возможностей.\n\n" +
+            "Можно реализовать команды из основного меню или типы команд.\n\n" +
+            "Type /start чтобы увидеть приветсятвеное сообщение.\n\n" +
+            "Type /mydata чтобы увидеть инфо о себе.\n\n" +
+            "Type /help увидеть это сообщение снова.";
 
     public TelegramBot(BotConfig config) {
         this.botConfig = config;
+        List<BotCommand> listOfCommands = new ArrayList<>();
+        listOfCommands.add(new BotCommand("/start", "получаем приветственое сообщение"));
+        listOfCommands.add(new BotCommand("/mydata", "get your data stored"));
+        listOfCommands.add(new BotCommand("/deletedata", "delete my data"));
+        listOfCommands.add(new BotCommand("/help", "how to use this bot"));
+        listOfCommands.add(new BotCommand("/settings", "set your preferences"));
+        try {
+            this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e) {
+            log.error("Error setting bot's command list: " + e.getMessage());
+        }
     }
 
     @Override
@@ -26,6 +50,10 @@ public class TelegramBot extends TelegramLongPollingBot {
                 case "/start":
                     startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
                     break;
+                case "/help":
+                    sendMessage(chatId, HELP_TEXT);
+                    break;
+
                 default:
                     sendMessage(chatId, "Sorry, command was't recognized  ");
             }
@@ -35,7 +63,7 @@ public class TelegramBot extends TelegramLongPollingBot {
     private void startCommandReceived(long chatId, String name) {
 
         String answer = "Hi, " + name + ", nice to meet you!";
-
+        log.info("Replied to user " + name);
         sendMessage(chatId, answer);
     }
 
@@ -47,7 +75,7 @@ public class TelegramBot extends TelegramLongPollingBot {
         try {
             execute(message);
         } catch (TelegramApiException e) {
-
+            log.error("Error occurred: " + e.getMessage());
         }
     }
 
