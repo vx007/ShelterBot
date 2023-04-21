e89549d38ef64a179894a8c62a86d4d9ce8e185b
AlexandrLytkin <Dracca@mail.ru>
2023-04-21 15:41:57 +0300

    добавил JavaDoc в сервисе

--- src/main/java/com/skypro/shelterbot/service/TelegramBot.java
+++ src/main/java/com/skypro/shelterbot/service/TelegramBot.java
@@ -7,6 +7,9 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+/**
+ * @extends {@link TelegramLongPollingBot} идет проверка не написали ли что то
+ */
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
 
@@ -16,6 +19,11 @@ public class TelegramBot extends TelegramLongPollingBot {
         this.botConfig = config;
     }
 
+    /**
+     * Update класс получает сообщение от пользователя боту,
+     * также хвранит всю всю инфо о нем
+     * @param update
+     */
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
@@ -32,13 +40,26 @@ public class TelegramBot extends TelegramLongPollingBot {
         }
     }
 
+    /**
+     * welcome message to new user
+     * @param chatId can't be <b>null</b>
+     * @param name can't be <b>null</b>
+     */
     private void startCommandReceived(long chatId, String name) {
 
         String answer = "Hi, " + name + ", nice to meet you!";
 
+        //TODO add check if user exists
         sendMessage(chatId, answer);
     }
 
+    /**
+     * send id users and text for him.
+     * <br>
+     * use method telegram's library {@link SendMessage message = new SendMessage();}
+     * @param chatId can't be <b>null</b>
+     *  @throws TelegramApiException can throw
+     */
     private void sendMessage(long chatId, String textToSend) {
         SendMessage message = new SendMessage();
         message.setChatId(String.valueOf(chatId));
