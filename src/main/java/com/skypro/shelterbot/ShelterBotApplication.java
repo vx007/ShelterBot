3d9e790fa60d8f5d80a12c78202c4cc6212b64ed
vx007 <vasilyev.x@mail.ru>
2023-04-26 23:06:50 +0300

    Total rebase

--- src/main/java/com/skypro/shelterbot/ShelterBotApplication.java
+++ src/main/java/com/skypro/shelterbot/ShelterBotApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class  ShelterBotApplication {
-
+public class ShelterBotApplication {
     public static void main(String[] args) {
-        SpringApplication.run (ShelterBotApplication.class,  args);
+        SpringApplication.run(ShelterBotApplication.class, args);
     }
-
 }
