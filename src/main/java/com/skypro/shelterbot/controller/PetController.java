9611c4555d66b35c6fcc147c072b9d449f8971b9
vx007 <vasilyev.x@mail.ru>
2023-05-15 19:38:19 +0300

    Изменены сервисы и контроллеры 15.05.2023 19:38

--- src/main/java/com/skypro/shelterbot/controller/PetController.java
+++ src/main/java/com/skypro/shelterbot/controller/PetController.java
@@ -1,27 +1,26 @@
 package com.skypro.shelterbot.controller;
 
+import com.skypro.shelterbot.exception.EntryNotFoundException;
 import com.skypro.shelterbot.model.Pet;
+import com.skypro.shelterbot.model.PetType;
 import com.skypro.shelterbot.service.PetService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import org.springframework.http.HttpStatus;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collection;
+import java.util.List;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("pets")
 public class PetController {
     private final PetService petService;
-
-    public PetController(PetService petService) {
-        this.petService = petService;
-    }
 
     @Operation(
             summary = "Добавление питомца в приют",
@@ -35,10 +34,6 @@ public class PetController {
                                             schema = @Schema(implementation = Pet.class)
                                     )
                             }
-                    ),
-                    @ApiResponse(
-                            responseCode = "400",
-                            description = "Если питомец уже находится в приюте"
                     )
             },
             requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
@@ -50,10 +45,10 @@ public class PetController {
                     }
             )
     )
-    @PostMapping()
-    public ResponseEntity<Pet> addPet(@Parameter(name = "питомец") @RequestBody Pet pet) {
-        Pet addPet = petService.add(pet);
-        return ResponseEntity.status(HttpStatus.CREATED).body(addPet);
+    @PostMapping("create")
+    public ResponseEntity<Pet> create(@Parameter(name = "питомец") @RequestBody Pet pet) {
+        var newPet = petService.add(pet);
+        return ResponseEntity.ok(newPet);
     }
 
     @Operation(
@@ -70,23 +65,19 @@ public class PetController {
                             }
                     ),
                     @ApiResponse(
-                            responseCode = "400",
+                            responseCode = "404",
                             description = "Если питомца нет"
                     )
-            },
-            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
-                    description = "имеющийся питомец",
-                    content = {
-                            @Content(
-                                    schema = @Schema(implementation = Pet.class)
-                            )
-                    }
-            )
+            }
     )
-    @GetMapping("{id}")
-    public ResponseEntity<Pet> findPet(@PathVariable Long id) {
-        Pet pet = petService.getById(id);
-        return ResponseEntity.ok(pet);
+    @GetMapping("by_id/{id}")
+    public ResponseEntity<Pet> readById(@PathVariable Long id) {
+        try {
+            var pet = petService.getById(id);
+            return ResponseEntity.ok(pet);
+        } catch (EntryNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @Operation(
@@ -103,14 +94,135 @@ public class PetController {
                             }
                     ),
                     @ApiResponse(
-                            responseCode = "400",
+                            responseCode = "404",
                             description = "Если питомцев нет"
                     )
             }
     )
-    @GetMapping("all_pet")
-    public ResponseEntity<Collection<Pet>> getAllPet() {
-        return ResponseEntity.ok(petService.getAll());
+    @GetMapping("all")
+    public ResponseEntity<List<Pet>> readAll() {
+        try {
+            var pets = petService.getAll();
+            return ResponseEntity.ok(pets);
+        } catch (EntryNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @Operation(
+            summary = "Изменить тип",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "тип изменён",
+                            content = {
+                                    @Content(
+                                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                            schema = @Schema(implementation = Pet.class)
+                                    )
+                            }
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Если пользователей нет"
+                    )
+            }
+    )
+    @PutMapping("update_type/{id}")
+    public ResponseEntity<Pet> updateType(@PathVariable Long id, @RequestBody PetType type) {
+        try {
+            var pet = petService.updateType(id, type);
+            return ResponseEntity.ok(pet);
+        } catch (EntryNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @Operation(
+            summary = "Изменить имя",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "имя изменено",
+                            content = {
+                                    @Content(
+                                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                            schema = @Schema(implementation = Pet.class)
+                                    )
+                            }
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Если пользователей нет"
+                    )
+            }
+    )
+    @PutMapping("update_name/{id}")
+    public ResponseEntity<Pet> updateName(@PathVariable Long id, @RequestBody String name) {
+        try {
+            var pet = petService.updateName(id, name);
+            return ResponseEntity.ok(pet);
+        } catch (EntryNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @Operation(
+            summary = "Изменить возраст",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "возраст изменён",
+                            content = {
+                                    @Content(
+                                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                            schema = @Schema(implementation = Pet.class)
+                                    )
+                            }
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Если пользователей нет"
+                    )
+            }
+    )
+    @PutMapping("update_age/{id}")
+    public ResponseEntity<Pet> updateAge(@PathVariable Long id, @RequestBody Integer age) {
+        try {
+            var pet = petService.updateAge(id, age);
+            return ResponseEntity.ok(pet);
+        } catch (EntryNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @Operation(
+            summary = "Изменить породу",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "порода изменена",
+                            content = {
+                                    @Content(
+                                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                            schema = @Schema(implementation = Pet.class)
+                                    )
+                            }
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Если пользователей нет"
+                    )
+            }
+    )
+    @PutMapping("update_breed/{id}")
+    public ResponseEntity<Pet> updateBreed(@PathVariable Long id, @RequestBody String breed) {
+        try {
+            var pet = petService.updateBreed(id, breed);
+            return ResponseEntity.ok(pet);
+        } catch (EntryNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @Operation(
@@ -127,14 +239,18 @@ public class PetController {
                             }
                     ),
                     @ApiResponse(
-                            responseCode = "400",
+                            responseCode = "404",
                             description = "Если питомца с таким id нет"
                     )
             }
     )
-    @DeleteMapping("{id}")
-    public ResponseEntity.BodyBuilder removePetById(@PathVariable Long id) {
-        petService.remove(id);
-        return ResponseEntity.ok();
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<Pet> removePetById(@PathVariable Long id) {
+        try {
+            petService.remove(id);
+            return ResponseEntity.ok().build();
+        } catch (EntryNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- src/main/java/com/skypro/shelterbot/service/UserService.java
+++ src/main/java/com/skypro/shelterbot/service/UserService.java
@@ -1,5 +1,7 @@
 package com.skypro.shelterbot.service;
 
+import com.skypro.shelterbot.exception.EntryAlreadyExists;
+import com.skypro.shelterbot.exception.EntryNotFoundException;
 import com.skypro.shelterbot.model.User;
 import com.skypro.shelterbot.repository.UserRepository;
 import lombok.NonNull;
@@ -15,55 +17,57 @@ public class UserService {
     private final UserRepository userRepository;
 
     @Transactional
-    public User add(@NonNull User user) {
+    public User add(@NonNull User user) throws EntryAlreadyExists {
         if (!userRepository.existsByChatId(user.getChatId())) {
             return userRepository.save(user);
         } else {
-            return null;
+            throw new EntryAlreadyExists("User already exists");
+        }
+    }
+
+    public User getByChatId(@NonNull Long chatId) throws EntryNotFoundException {
+        return userRepository.findByChatId(chatId).orElseThrow(() -> new EntryNotFoundException("User not found by chatID"));
+    }
+
+    public User getByPetId(@NonNull Long petId) throws EntryNotFoundException {
+        return userRepository.findByPetId(petId).orElseThrow(() -> new EntryNotFoundException("User not found by petID"));
+    }
+
+    public List<User> getAll() throws EntryNotFoundException {
+        if (!userRepository.findAll().isEmpty()){
+            return userRepository.findAll();
+        } else {
+            throw new EntryNotFoundException("Users not found");
         }
     }
 
     @Transactional
-    public User getByChatId(@NonNull Long chatId) {
-        return userRepository.findByChatId(chatId);
-    }
-
-    public User getByPetId(@NonNull Long petId) {
-        return userRepository.findByPetId(petId);
-    }
-
-    public List<User> getAll() {
-        return userRepository.findAll();
-    }
-
-    @Transactional
-
-    public User updateName(@NonNull Long chatId, String name) {
-        var user = userRepository.findByChatId(chatId);
+    public User updateName(@NonNull Long chatId, String name) throws EntryNotFoundException {
+        var user = getByChatId(chatId);
         user.setName(name);
         return userRepository.save(user);
     }
 
     @Transactional
-
-    public User updatePhone(@NonNull Long chatId, String phone) {
-        var user = userRepository.findByChatId(chatId);
+    public User updatePhone(@NonNull Long chatId, String phone) throws EntryNotFoundException {
+        var user = getByChatId(chatId);
         user.setPhone(phone);
         return userRepository.save(user);
     }
 
     @Transactional
-    public User updateLastCommand(@NonNull Long chatId, String command) {
-        var user = userRepository.findByChatId(chatId);
+    public User updateLastCommand(@NonNull Long chatId, String command) throws EntryNotFoundException {
+        var user = getByChatId(chatId);
         user.setLastCommand(command);
         return userRepository.save(user);
     }
 
     @Transactional
-    public void remove(@NonNull Long chatId) {
+    public void remove(@NonNull Long chatId) throws EntryNotFoundException {
         if (userRepository.existsByChatId(chatId)) {
             userRepository.deleteByChatId(chatId);
+        } else {
+            throw new EntryNotFoundException("User not found by chatID");
         }
     }
-
 }
