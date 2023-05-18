67d2d3cb9d834083a8dffb4421cf07859262531b
Dianakyz <3222777my@gmail.com>
2023-05-18 20:50:28 +0300

    тесты на контроллеры

--- src/test/java/com/skypro/shelterbot/controller/UserControllerTest.java
+++ src/test/java/com/skypro/shelterbot/controller/UserControllerTest.java
@@ -1,28 +1,27 @@
 package com.skypro.shelterbot.controller;
 
-import com.skypro.shelterbot.model.Pet;
-import com.skypro.shelterbot.model.Report;
+import com.skypro.shelterbot.exception.EntryAlreadyExists;
+import com.skypro.shelterbot.exception.EntryNotFoundException;
 import com.skypro.shelterbot.model.User;
 
 import com.skypro.shelterbot.service.UserService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-
-import java.sql.Timestamp;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.*;
 
 @WebMvcTest(controllers = UserController.class)
 public class UserControllerTest {
@@ -40,84 +39,269 @@ public class UserControllerTest {
 
 
     @Test
-    public void getAllUsersPositiveTest() throws Exception{
+    public void create_ValidUser_ShouldReturnNewUser() throws EntryAlreadyExists {
+        UserService userService = mock(UserService.class);
+        UserController userController = new UserController(userService);
 
-        final Long chatId = 232L;
-        final String name = "Diana";
-        final String phone = "userPhone";
-        final String lastCommand = "lastCommand";
-        final Timestamp registeredAt = new Timestamp(1L);
+        User user = new User();
+        when(userService.add(user)).thenReturn(user);
 
-        Pet pet = new Pet();
-        pet.setAge(3);
-        pet.setName("Toti");
+        ResponseEntity<User> response = userController.create(user);
 
-        final List<Report> reports = new ArrayList<>();
-
-        User user = new User(232L, "Diana", "userPhone", "lastCommand", registeredAt, pet, reports);
-        List<User> users = new ArrayList<>(List.of(user));
-
-        Mockito.when(userService.getAll()).thenReturn(users);
-
-        mockMvc.perform(MockMvcRequestBuilders
-                        .get("/users/all_users")
-                        .contentType(MediaType.APPLICATION_JSON_VALUE)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].chatId").value(chatId))
-                .andExpect(jsonPath("$[0].name").value(name))
-                .andExpect(jsonPath("$[0].phone").value(phone))
-                .andExpect(jsonPath("$[0].lastCommand").value(lastCommand))
-                .andExpect(jsonPath("$[0].registeredAt").value(registeredAt))
-                .andExpect(jsonPath("$[0].pet").value(pet))
-                .andExpect(jsonPath("$[0].reports").value(reports));
-
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(user, response.getBody());
     }
 
     @Test
-    public void getAllUsersTestWhenReturnEmptyList() throws Exception{
+    public void create_UserAlreadyExists_ShouldReturnNotAcceptable() throws EntryAlreadyExists {
+        UserService userService = mock(UserService.class);
+        UserController userController = new UserController(userService);
 
-        Mockito.when(userService.getAll()).thenReturn(Collections.emptyList());
+        User user = new User();
+        when(userService.add(user)).thenThrow(EntryAlreadyExists.class);
 
-        mockMvc.perform(MockMvcRequestBuilders
-                        .get("/users/all_users")
-                        .contentType(MediaType.APPLICATION_JSON_VALUE)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isNotFound());
+        ResponseEntity<User> response = userController.create(user);
+
+        assertEquals(HttpStatus.NOT_ACCEPTABLE, response.getStatusCode());
+        assertNull(response.getBody());
     }
 
     @Test
-    public void findUserByPetPositiveTest() throws Exception{
+    public void readByChatId_ExistingChatId_ShouldReturnUser() throws EntryNotFoundException {
+        UserService userService = mock(UserService.class);
+        UserController userController = new UserController(userService);
 
-        final Long chatId = 232L;
-        final String name = "Diana";
-        final String phone = "userPhone";
-        final String lastCommand = "lastCommand";
-        final Timestamp registeredAt = new Timestamp(1L);
+        long chatId = 123;
+        User user = new User();
+        when(userService.getByChatId(chatId)).thenReturn(user);
 
-        Pet pet = new Pet();
-        pet.setAge(3);
-        pet.setName("Toti");
+        ResponseEntity<User> response = userController.readByChatId(chatId);
 
-        final List<Report> reports = new ArrayList<>();
-
-        User user = new User(232L, "Diana", "userPhone", "lastCommand", registeredAt, pet, reports);
-        List<User> users = new ArrayList<>(List.of(user));
-
-        Mockito.when(userService.getByPetId(chatId)).thenReturn((User) users);
-
-        mockMvc.perform(MockMvcRequestBuilders
-                        .get("/users/pet?pet={pet}", pet)
-                        .contentType(MediaType.APPLICATION_JSON_VALUE)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].chatId").value(chatId))
-                .andExpect(jsonPath("$[0].name").value(name))
-                .andExpect(jsonPath("$[0].phone").value(phone))
-                .andExpect(jsonPath("$[0].lastCommand").value(lastCommand))
-                .andExpect(jsonPath("$[0].registeredAt").value(registeredAt))
-                .andExpect(jsonPath("$[0].pet").value(pet))
-                .andExpect(jsonPath("$[0].reports").value(reports));
-
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(user, response.getBody());
     }
+
+    @Test
+    public void readByChatId_NonExistingChatId_ShouldReturnNotFound() throws EntryNotFoundException {
+        UserService userService = mock(UserService.class);
+        UserController userController = new UserController(userService);
+
+        long chatId = 123;
+        when(userService.getByChatId(chatId)).thenThrow(EntryNotFoundException.class);
+
+        ResponseEntity<User> response = userController.readByChatId(chatId);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertNull(response.getBody());
+    }
+
+    @Test
+    public void readByPetId_ExistingPetId_ShouldReturnUser() throws EntryNotFoundException {
+        UserService userService = mock(UserService.class);
+        UserController userController = new UserController(userService);
+
+        long petId = 123;
+        User user = new User();
+        when(userService.getByPetId(petId)).thenReturn(user);
+
+        ResponseEntity<User> response = userController.readByPetId(petId);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(user, response.getBody());
+    }
+
+    @Test
+    public void readByPetId_NonExistingPetId_ShouldReturnNotFound() throws EntryNotFoundException {
+        UserService userService = mock(UserService.class);
+        UserController userController = new UserController(userService);
+
+        long petId = 123;
+        when(userService.getByPetId(petId)).thenThrow(EntryNotFoundException.class);
+
+        ResponseEntity<User> response = userController.readByPetId(petId);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertNull(response.getBody());
+    }
+
+    @Test
+    public void readAll_ExistingUsers_ShouldReturnListOfUsers() throws EntryNotFoundException {
+        UserService userService = mock(UserService.class);
+        UserController userController = new UserController(userService);
+
+        List<User> users = new ArrayList<>();
+        users.add(new User());
+        users.add(new User());
+        when(userService.getAll()).thenReturn(users);
+
+        ResponseEntity<List<User>> response = userController.readAll();
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(users, response.getBody());
+    }
+
+    @Test
+    public void readAll_NoUsersFound_ShouldReturnNotFound() throws EntryNotFoundException {
+        UserService userService = mock(UserService.class);
+        UserController userController = new UserController(userService);
+
+        when(userService.getAll()).thenThrow(EntryNotFoundException.class);
+
+        ResponseEntity<List<User>> response = userController.readAll();
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertNull(response.getBody());
+    }
+
+    @Test
+    public void updateName_ExistingChatId_ShouldReturnUpdatedUser() throws EntryNotFoundException {
+        UserService userService = mock(UserService.class);
+        UserController userController = new UserController(userService);
+
+        long chatId = 123;
+        String newName = "New Name";
+        User user = new User();
+        when(userService.updateName(chatId, newName)).thenReturn(user);
+
+        ResponseEntity<User> response = userController.updateName(chatId, newName);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(user, response.getBody());
+    }
+
+    @Test
+    public void updateName_NonExistingChatId_ShouldReturnNotFound() throws EntryNotFoundException {
+        UserService userService = mock(UserService.class);
+        UserController userController = new UserController(userService);
+
+        long chatId = 123;
+        String newName = "New Name";
+        when(userService.updateName(chatId, newName)).thenThrow(EntryNotFoundException.class);
+
+        ResponseEntity<User> response = userController.updateName(chatId, newName);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertNull(response.getBody());
+    }
+
+    @Test
+    public void updatePhone_ExistingChatId_ShouldReturnUpdatedUser() throws EntryNotFoundException {
+        UserService userService = mock(UserService.class);
+        UserController userController = new UserController(userService);
+
+        long chatId = 123;
+        String newPhone = "1234567890";
+        User user = new User();
+        when(userService.updatePhone(chatId, newPhone)).thenReturn(user);
+
+        ResponseEntity<User> response = userController.updatePhone(chatId, newPhone);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(user, response.getBody());
+    }
+
+    @Test
+    public void updatePhone_NonExistingChatId_ShouldReturnNotFound() throws EntryNotFoundException {
+        UserService userService = mock(UserService.class);
+        UserController userController = new UserController(userService);
+
+        long chatId = 123;
+        String newPhone = "1234567890";
+        when(userService.updatePhone(chatId, newPhone)).thenThrow(EntryNotFoundException.class);
+
+        ResponseEntity<User> response = userController.updatePhone(chatId, newPhone);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertNull(response.getBody());
+    }
+
+    @Test
+    public void updateLastCommand_ExistingChatId_ShouldReturnUpdatedUser() throws EntryNotFoundException {
+        UserService userService = mock(UserService.class);
+        UserController userController = new UserController(userService);
+
+        long chatId = 123;
+        String newCommand = "/start";
+        User user = new User();
+        when(userService.updateLastCommand(chatId, newCommand)).thenReturn(user);
+
+        ResponseEntity<User> response = userController.updateLastCommand(chatId, newCommand);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(user, response.getBody());
+    }
+
+    @Test
+    public void updateLastCommand_NonExistingChatId_ShouldReturnNotFound() throws EntryNotFoundException {
+        UserService userService = mock(UserService.class);
+        UserController userController = new UserController(userService);
+
+        long chatId = 123;
+        String newCommand = "/start";
+        when(userService.updateLastCommand(chatId, newCommand)).thenThrow(EntryNotFoundException.class);
+
+        ResponseEntity<User> response = userController.updateLastCommand(chatId, newCommand);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertNull(response.getBody());
+    }
+
+    @Test
+    public void updatePet_ExistingChatIdAndPetId_ShouldReturnUpdatedUser() throws EntryNotFoundException {
+        UserService userService = mock(UserService.class);
+        UserController userController = new UserController(userService);
+
+        long chatId = 123;
+        long petId = 456;
+        User user = new User();
+        when(userService.updatePet(chatId, petId)).thenReturn(user);
+
+        ResponseEntity<User> response = userController.updatePet(chatId, petId);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(user, response.getBody());
+    }
+
+    @Test
+    public void updatePet_NonExistingChatIdOrPetId_ShouldReturnNotFound() throws EntryNotFoundException {
+        UserService userService = mock(UserService.class);
+        UserController userController = new UserController(userService);
+
+        long chatId = 123;
+        long petId = 456;
+        when(userService.updatePet(chatId, petId)).thenThrow(EntryNotFoundException.class);
+
+        ResponseEntity<User> response = userController.updatePet(chatId, petId);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertNull(response.getBody());
+    }
+
+    @Test
+    public void delete_ExistingChatId_ShouldReturnOk() throws EntryNotFoundException {
+        UserService userService = mock(UserService.class);
+        UserController userController = new UserController(userService);
+
+        long chatId = 123;
+
+        ResponseEntity<User> response = userController.delete(chatId);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNull(response.getBody());
+    }
+
+    @Test
+    public void delete_NonExistingChatId_ShouldReturnNotFound() throws EntryNotFoundException {
+        UserService userService = mock(UserService.class);
+        UserController userController = new UserController(userService);
+
+        long chatId = 123;
+        doThrow(EntryNotFoundException.class).when(userService).remove(chatId);
+
+        ResponseEntity<User> response = userController.delete(chatId);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertNull(response.getBody());
+    }
+
 }
